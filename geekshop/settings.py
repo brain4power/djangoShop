c2118446183a37c01dc7a4d508404f5dc4c7a70e
brain4power <yotakrasnodar@yandex.ru>
2019-04-04 20:45:34 +0300

    скорректированы настройки под продакшн, удалены f строки

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -197,9 +197,3 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 )
-
-
-try:
-    from .production_settings import *
-except ImportError:
-    pass
