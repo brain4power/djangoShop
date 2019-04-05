efb86f8102342ab60dfecd4c845de76b9a2c4131
brain4power <yotakrasnodar@yandex.ru>
2019-04-05 12:27:25 +0300

    переделал урлы в authapp с url на path

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,13 +1,12 @@
-from django.conf.urls import url
 from django.urls import path
 import authapp.views as authapp
 
 app_name = 'authapp'
 
 urlpatterns = [
-    url(r'^login/$', authapp.login, name='login'),
-    url(r'^logout/$', authapp.logout, name='logout'),
-    url(r'^register/$', authapp.register, name='register'),
-    url(r'^edit/$', authapp.edit, name='edit'),
+    path('login/', authapp.login, name='login'),
+    path('logout/', authapp.logout, name='logout'),
+    path('register/', authapp.register, name='register'),
+    path('edit/', authapp.edit, name='edit'),
     path('verify/<str:email>/<str:activation_key>/', authapp.verify, name='verify'),
 ]
--- basketapp/urls.py
+++ basketapp/urls.py
@@ -3,7 +3,6 @@ import basketapp.views as basketapp
 
 app_name = 'basketapp'
 
-
 urlpatterns = [
     re_path(r'^$', basketapp.basket, name='view'),
     re_path(r'^add/(?P<pk>\d+)/$', basketapp.basket_add, name='add'),
