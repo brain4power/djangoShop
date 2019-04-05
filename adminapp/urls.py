80836616e19630d2edce58fe8413e43c19f569ec
brain4power <yotakrasnodar@yandex.ru>
2019-04-05 12:29:20 +0300

    переделал урлы в adminapp с url на path и re_path

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,5 +1,4 @@
-from django.conf.urls import url
-from django.urls import path
+from django.urls import path, re_path
 from .views import categories
 import adminapp.views as adminapp
 
@@ -7,20 +6,19 @@ app_name = 'adminapp'
 
 urlpatterns = [
     path('', categories, name='admin'),
-    url(r'^users/create/$', adminapp.user_create, name='user_create'),
-    #url(r'^users/read/$', adminapp.users, name='users'),
+    path('users/create/', adminapp.user_create, name='user_create'),
     path('users/read/', adminapp.UsersListView.as_view(), name='users'),
-    url(r'^users/update/(?P<pk>\d+)/$', adminapp.user_update, name='user_update'),
-    url(r'^users/delete/(?P<pk>\d+)/$', adminapp.user_delete, name='user_delete'),
-    
-    url(r'^categories/create/$', adminapp.category_create, name='category_create'),
-    url(r'^categories/read/$', adminapp.categories, name='categories'),
-    url(r'^categories/update/(?P<pk>\d+)/$', adminapp.category_update, name='category_update'),
-    url(r'^categories/delete/(?P<pk>\d+)/$', adminapp.category_delete, name='category_delete'),
-    
-    url(r'^products/create/category/(?P<pk>\d+)/$', adminapp.product_create, name='product_create'),
-    url(r'^products/read/category/(?P<pk>\d+)/$', adminapp.products, name='products'),
-    url(r'^products/read/(?P<pk>\d+)/$', adminapp.product_read, name='product_read'),
-    url(r'^products/update/(?P<pk>\d+)/$', adminapp.product_update, name='product_update'),
-    url(r'^products/delete/(?P<pk>\d+)/$', adminapp.product_delete, name='product_delete'), 
+    re_path(r'^users/update/(?P<pk>\d+)/$', adminapp.user_update, name='user_update'),
+    re_path(r'^users/delete/(?P<pk>\d+)/$', adminapp.user_delete, name='user_delete'),
+
+    path('categories/create/', adminapp.category_create, name='category_create'),
+    path('categories/read/', adminapp.categories, name='categories'),
+    re_path(r'^categories/update/(?P<pk>\d+)/$', adminapp.category_update, name='category_update'),
+    re_path(r'^categories/delete/(?P<pk>\d+)/$', adminapp.category_delete, name='category_delete'),
+
+    re_path(r'^products/create/category/(?P<pk>\d+)/$', adminapp.product_create, name='product_create'),
+    re_path(r'^products/read/category/(?P<pk>\d+)/$', adminapp.products, name='products'),
+    re_path(r'^products/read/(?P<pk>\d+)/$', adminapp.product_read, name='product_read'),
+    re_path(r'^products/update/(?P<pk>\d+)/$', adminapp.product_update, name='product_update'),
+    re_path(r'^products/delete/(?P<pk>\d+)/$', adminapp.product_delete, name='product_delete'),
 ]
