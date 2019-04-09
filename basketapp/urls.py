7d66632f0845907329cd0e350b6954ccc71088ce
brain4power <yotakrasnodar@yandex.ru>
2019-04-09 13:58:42 +0300

    в корзине теперь можно добавлять количество товара, а финальные стоимости сами пересчитываются через ajax

--- basketapp/urls.py
+++ basketapp/urls.py
@@ -1,4 +1,4 @@
-from django.urls import re_path
+from django.urls import re_path, path
 import basketapp.views as basketapp
 
 app_name = 'basketapp'
@@ -7,4 +7,6 @@ urlpatterns = [
     re_path(r'^$', basketapp.basket, name='view'),
     re_path(r'^add/(?P<pk>\d+)/$', basketapp.basket_add, name='add'),
     re_path(r'^remove/(?P<pk>\d+)/$', basketapp.basket_remove, name='remove'),
+    path('edit/<int:pk>/<int:quantity>/', basketapp.basket_edit, name='edit')
+
 ]
